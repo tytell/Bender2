bb90920c7d2e6b77f8bf6f4b66eb634eb2be062a
Eric Tytell <eric.tytell@tufts.edu>
2017-06-13 10:10:03 -0400

    Working on setting up for ergometer experiments

--- bender.py
+++ bender.py
@@ -20,56 +20,19 @@ from bender_ui import Ui_BenderWindow
 from benderdaq import BenderDAQ
 from benderfile import BenderFile
 
-from settings import SETTINGS_FILE, MOTOR_TYPE, COUNTER_TYPE
+from settings import SETTINGS_FILE, MOTOR_TYPE, COUNTER_TYPE, EXPERIMENT_TYPE
+
+if EXPERIMENT_TYPE == 'isolated muscle':
+    from ergometer_params import stimParameterDefs, setup_parameters
+elif EXPERIMENT_TYPE == 'whole body':
+    from wholebody_params import stimParameterDefs, setup_parameters
+else:
+    raise ValueError('Unknown experiment type {}'.format(EXPERIMENT_TYPE))
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
 
 
-stimParameterDefs = {
-    'None': [
-        {'name': 'Duration', 'type': 'float', 'value': 5.0, 'step': 1.0, 'suffix': 'sec'}
-    ],
-    'Sine': [
-        {'name': 'Amplitude', 'type': 'float', 'value': 15.0, 'step': 1.0, 'suffix': 'deg'},
-        {'name': 'Frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
-        {'name': 'Cycles', 'type': 'int', 'value': 10},
-        {'name': 'Activation', 'type': 'group', 'children': [
-            {'name': 'On', 'type': 'bool', 'value': True},
-            {'name': 'Start cycle', 'type': 'int', 'value': 3},
-            {'name': 'Phase', 'type': 'float', 'value': 0.0, 'step': 10.0, 'suffix': '%'},
-            {'name': 'Duty', 'type': 'float', 'value': 30.0, 'step': 10.0, 'suffix': '%'},
-            {'name': 'Left voltage', 'type': 'float', 'value': 2.0, 'step': 1.0, 'suffix': 'V'},
-            {'name': 'Left voltage scale', 'type': 'float', 'value': 1.0, 'step': 1.0, 'suffix': 'V/V'},
-            {'name': 'Right voltage', 'type': 'float', 'value': 2.0, 'step': 1.0, 'suffix': 'V'},
-            {'name': 'Right voltage scale', 'type': 'float', 'value': 0.4, 'step': 1.0, 'suffix': 'V/V'},
-            {'name': 'Pulse rate', 'type': 'float', 'value': 75.0, 'step': 5.0, 'suffix': 'Hz'},
-        ]}
-    ],
-    'Frequency Sweep': [
-        {'name': 'Start frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
-        {'name': 'End frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
-        {'name': 'Frequency change', 'type': 'list', 'values': ['Exponential','Linear'], 'value': 'Exponential'},
-        {'name': 'Duration', 'type': 'float', 'value': 300.0, 'suffix': 'sec'},
-        {'name': 'Amplitude', 'type': 'float', 'value': 15.0, 'step': 1.0, 'suffix': 'deg'},
-        {'name': 'Frequency exponent', 'type': 'float', 'value': 0.0, 'limits': (-1, 0)}
-    ],
-    'Ramp': [
-        {'name': 'Amplitude', 'type': 'float', 'value': 10.0, 'step': 1.0, 'suffix': 'deg'},
-        {'name': 'Rate', 'type': 'float', 'value': 50.0, 'step': 10.0, 'suffix': 'deg/s'},
-        {'name': 'Hold duration', 'type': 'float', 'value': 2.0, 'step': 0.5, 'suffix': 'sec'},
-        {'name': 'Activation', 'type': 'group', 'children': [
-            {'name': 'During', 'type': 'list', 'values': ['Hold', 'Ramp'], 'value': 'Hold'},
-            {'name': 'Duration', 'type': 'float', 'value': 0.3, 'step': 0.1, 'suffix': 'sec'},
-            {'name': 'Delay', 'type': 'float', 'value': 0.05, 'step': 0.01, 'suffix': 'sec', 'siPrefix': True},
-            {'name': 'Stim side', 'type': 'list', 'values': ['Left', 'Right'], 'value': 'Left'},
-            {'name': 'Stim voltage', 'type': 'float', 'value': 2.0, 'step': 1.0, 'suffix': 'V'},
-            {'name': 'Left voltage scale', 'type': 'float', 'value': 1.0, 'step': 1.0, 'suffix': 'V/V'},
-            {'name': 'Right voltage scale', 'type': 'float', 'value': 0.4, 'step': 1.0, 'suffix': 'V/V'},
-            {'name': 'Pulse rate', 'type': 'float', 'value': 75.0, 'step': 5.0, 'suffix': 'Hz'},
-        ]}
-    ]
-}
 
 fileNameTips = {
     'None': '''{tp}: None''',
@@ -93,82 +56,6 @@ fileNameTips = {
 {num}: Trial number'''
 }
 
-velocityDriverParams = [
-    {'name': 'Maximum speed', 'type': 'float', 'value': 400.0, 'step': 50.0, 'suffix': 'RPM'},
-    {'name': 'Minimum pulse frequency', 'type': 'float', 'value': 1000.0, 'step': 100.0, 'siPrefix': True,
-     'suffix': 'Hz'},
-    {'name': 'Maximum pulse frequency', 'type': 'float', 'value': 5000.0, 'step': 100.0, 'siPrefix': True,
-     'suffix': 'Hz'},
-    {'name': 'Sign convention', 'type': 'list', 'values': ['Left is positive', 'Left is negative', 'None'],
-     'value': 'Left is positive'}
-]
-
-stepperParams = [
-    {'name': 'Steps per revolution', 'type': 'float', 'value': 6400},
-    {'name': 'Sign convention', 'type': 'list', 'values': ['Left is positive', 'Left is negative', 'None'],
-     'value': 'Left is positive'}
-]
-
-encoderParams = [
-    {'name': 'Encoder', 'type': 'str', 'value': 'Dev1/ctr0'},
-    {'name': 'Counts per revolution', 'type': 'int', 'value': 10000, 'limits': (1, 100000)},
-    {'name': 'Sign convention', 'type': 'list', 'values': ['Left is positive', 'Left is negative', 'None'],
-     'value': 'Left is positive'}
-]
-
-pwmParams = [
-    {'name': 'Pulse width counter', 'type': 'str', 'value': 'Dev1/ctr0'},
-    {'name': 'Base frequency', 'type': 'float', 'value': 500, 'suffix': 'Hz'}
-]
-
-parameterDefinitions = [
-    {'name': 'DAQ', 'type': 'group', 'children': [
-        {'name': 'Input', 'type': 'group', 'children': [
-            {'name': 'Sampling frequency', 'type': 'float', 'value': 1000.0, 'step': 500.0, 'siPrefix': True,
-             'suffix': 'Hz'},
-            {'name': 'xForce', 'type': 'str', 'value': 'Dev1/ai0'},
-            {'name': 'yForce', 'type': 'str', 'value': 'Dev1/ai1'},
-            {'name': 'zForce', 'type': 'str', 'value': 'Dev1/ai2'},
-            {'name': 'xTorque', 'type': 'str', 'value': 'Dev1/ai3'},
-            {'name': 'yTorque', 'type': 'str', 'value': 'Dev1/ai4'},
-            {'name': 'zTorque', 'type': 'str', 'value': 'Dev1/ai5'},
-            {'name': 'Left stim', 'type': 'str', 'value': 'Dev1/ai6'},
-            {'name': 'Right stim', 'type': 'str', 'value': 'Dev1/ai7'},
-            {'name': 'Get calibration...', 'type': 'action'},
-            {'name': 'Calibration file', 'type': 'str', 'readonly': True}
-        ]},
-        {'name': 'Output', 'type': 'group', 'children': [
-            {'name': 'Sampling frequency', 'type': 'float', 'value': 10000.0, 'step': 1000.0, 'siPrefix': True,
-             'suffix': 'Hz', 'readonly': True},
-            {'name': 'Left stimulus', 'type': 'str', 'value': 'Dev1/ao0'},
-            {'name': 'Right stimulus', 'type': 'str', 'value': 'Dev1/ao1'},
-            {'name': 'Digital port', 'type': 'str', 'value': 'Dev1/port0'}
-        ]},
-        {'name': 'Update rate', 'type': 'float', 'value': 10.0, 'suffix': 'Hz'}
-    ]},
-    {'name': 'Motor parameters', 'type': 'group', 'children': []},
-    {'name': 'Geometry', 'type': 'group', 'children': [
-        {'name': 'doutvert', 'tip': 'Vertical distance from transducer to center of pressure', 'type': 'float',
-         'value': 0.011, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-        {'name': 'douthoriz', 'tip': 'Horizontal distance from transducer to center of pressure', 'type': 'float',
-         'value': 0.0, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-        {'name': 'din', 'tip': 'Horizontal distance from center of pressure to center of rotation', 'type': 'float',
-         'value': 0.035, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-        {'name': 'dclamp', 'tip': 'Horizontal distance between the edges of the clamps', 'type': 'float',
-         'value': 0.030, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-        {'name': 'Cross-section', 'type': 'group', 'children': [
-            {'name': 'width', 'type': 'float', 'value': 0.021, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-            {'name': 'height', 'type': 'float', 'value': 0.021, 'step': 0.001, 'siPrefix': True, 'suffix': 'm'},
-        ]}
-    ]},
-    {'name': 'Stimulus', 'type': 'group', 'children': [
-        {'name': 'Type', 'type': 'list', 'values': ['None', 'Sine', 'Frequency Sweep', 'Ramp'], 'value': 'Sine'},
-        {'name': 'Parameters', 'type': 'group', 'children': stimParameterDefs['Sine']},
-        {'name': 'Wait before', 'type': 'float', 'value': 1.0, 'suffix': 's'},
-        {'name': 'Wait after', 'type': 'float', 'value': 1.0, 'suffix': 's'},
-    ]}
-]
-
 # to update the UI -
 # run Designer.exe to modify,
 # then python C:\Anaconda2\Lib\site-packages\PyQt4\uic\pyuic.py bender.ui -o bender_ui.py
@@ -189,25 +76,7 @@ class BenderWindow(QtGui.QMainWindow):
         self.params = Parameter.create(name='params', type='group', children=parameterDefinitions)
         self.ui.parameterTreeWidget.setParameters(self.params, showTop=False)
 
-        if MOTOR_TYPE == 'stepper':
-            self.params.child('Motor parameters').addChildren(stepperParams)
-            self.params.child('DAQ', 'Output', 'Sampling frequency').setWritable()
-            self.params['DAQ', 'Output', 'Sampling frequency'] = 100000
-        elif MOTOR_TYPE == 'velocity':
-            self.params.child('Motor parameters').addChildren(velocityDriverParams)
-        elif MOTOR_TYPE == 'none':
-            self.params.child('Motor parameters').remove()
-            self.params.child('Stimulus', 'Type').setValues(['None'])
-            self.params.child('Stimulus', 'Type').setValue('None')
-        else:
-            raise ValueError('Unknown MOTOR_TYPE {}'.format(MOTOR_TYPE))
-
-        if COUNTER_TYPE == 'encoder':
-            self.params.child('DAQ', 'Input').addChildren(encoderParams)
-        elif COUNTER_TYPE == 'pwm':
-            self.params.child('DAQ', 'Input').addChildren(pwmParams)
-        else:
-            raise ValueError('Unknown COUNTER_TYPE {}'.format(COUNTER_TYPE))
+        setup_parameters(self.params)
 
         stimtype = self.params.child('Stimulus', 'Type')
         self.curStimType = stimtype.value()
@@ -245,35 +114,6 @@ class BenderWindow(QtGui.QMainWindow):
         ui = Ui_BenderWindow()
         ui.setupUi(self)
         self.ui = ui
-
-    def connectParameterSlots(self):
-        self.params.child('Stimulus', 'Type').sigValueChanged.connect(self.changeStimType)
-        self.params.child('Stimulus').sigTreeStateChanged.connect(self.generateStimulus)
-        self.params.child('DAQ', 'Update rate').sigValueChanged.connect(self.generateStimulus)
-        if MOTOR_TYPE == 'velocity':
-            self.params.child('Motor parameters', 'Maximum pulse frequency').sigValueChanged.connect(self.updateOutputFrequency)
-        elif MOTOR_TYPE == 'stepper':
-            self.params.child('DAQ', 'Output', 'Sampling frequency').sigValueChanged.connect(self.generateStimulus)
-
-        self.params.child('Motor parameters').sigTreeStateChanged.connect(self.generateStimulus)
-        self.params.child('DAQ', 'Input', 'Get calibration...').sigActivated.connect(self.getCalibration)
-
-    def disconnectParameterSlots(self):
-        try:
-            self.params.child('Stimulus', 'Type').sigValueChanged.disconnect(self.changeStimType)
-            self.params.child('Stimulus').sigTreeStateChanged.disconnect(self.generateStimulus)
-            self.params.child('DAQ', 'Update rate').sigValueChanged.disconnect(self.generateStimulus)
-            if MOTOR_TYPE == 'velocity':
-                self.params.child('Motor parameters', 'Maximum pulse frequency').sigValueChanged.disconnect(
-                    self.updateOutputFrequency)
-            elif MOTOR_TYPE == 'stepper':
-                self.params.child('DAQ', 'Output', 'Sampling frequency').sigValueChanged.disconnect(self.generateStimulus)
-
-            self.params.child('Motor parameters').sigTreeStateChanged.disconnect(self.generateStimulus)
-            self.params.child('DAQ', 'Input', 'Get calibration...').sigActivated.disconnect(self.getCalibration)
-        except TypeError:
-            logging.warning('Problem disconnecting parameter slots')
-            pass
 
     def startAcquisition(self):
         if self.calibration is None or self.calibration.size == 0:
---
+++ ergometer_params.py
@@ -0,0 +1,93 @@
+import pyqtgraph.parametertree.parameterTypes as pTypes
+from pyqtgraph.parametertree import Parameter, ParameterTree, ParameterItem, registerParameterType
+
+stimParameterDefs = {
+    'None': [
+        {'name': 'Duration', 'type': 'float', 'value': 5.0, 'step': 1.0, 'suffix': 'sec'}
+    ],
+    'Sine': [
+        {'name': 'Amplitude', 'type': 'float', 'value': 15.0, 'step': 1.0, 'suffix': 'deg'},
+        {'name': 'Frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
+        {'name': 'Cycles', 'type': 'int', 'value': 10},
+        {'name': 'Activation', 'type': 'group', 'children': [
+            {'name': 'On', 'type': 'bool', 'value': True},
+            {'name': 'Start cycle', 'type': 'int', 'value': 3},
+            {'name': 'Phase', 'type': 'float', 'value': 0.0, 'step': 10.0, 'suffix': '%'},
+            {'name': 'Type', 'type': 'list', 'values': ['Sync pulse', 'Generate train'], 'value': 'Sync pulse'},
+            {'name': 'Duty', 'type': 'float', 'value': 30.0, 'step': 10.0, 'suffix': '%'},
+            {'name': 'Voltage', 'type': 'float', 'value': 2.0, 'step': 1.0, 'suffix': 'V'},
+            {'name': 'Voltage scale', 'type': 'float', 'value': 1.0, 'step': 1.0, 'suffix': 'V/V'},
+            {'name': 'Pulse rate', 'type': 'float', 'value': 75.0, 'step': 5.0, 'suffix': 'Hz'},
+        ]}
+    ],
+    'Frequency Sweep': [
+        {'name': 'Start frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
+        {'name': 'End frequency', 'type': 'float', 'value': 1.0, 'step': 0.1, 'suffix': 'Hz'},
+        {'name': 'Frequency change', 'type': 'list', 'values': ['Exponential','Linear'], 'value': 'Exponential'},
+        {'name': 'Duration', 'type': 'float', 'value': 300.0, 'suffix': 'sec'},
+        {'name': 'Amplitude', 'type': 'float', 'value': 15.0, 'step': 1.0, 'suffix': 'deg'},
+        {'name': 'Frequency exponent', 'type': 'float', 'value': 0.0, 'limits': (-1, 0)}
+    ],
+    'Ramp': [
+        {'name': 'Amplitude', 'type': 'float', 'value': 10.0, 'step': 1.0, 'suffix': 'deg'},
+        {'name': 'Rate', 'type': 'float', 'value': 50.0, 'step': 10.0, 'suffix': 'deg/s'},
+        {'name': 'Hold duration', 'type': 'float', 'value': 2.0, 'step': 0.5, 'suffix': 'sec'},
+        {'name': 'Activation', 'type': 'group', 'children': [
+            {'name': 'During', 'type': 'list', 'values': ['Hold', 'Ramp'], 'value': 'Hold'},
+            {'name': 'Duration', 'type': 'float', 'value': 0.3, 'step': 0.1, 'suffix': 'sec'},
+            {'name': 'Delay', 'type': 'float', 'value': 0.05, 'step': 0.01, 'suffix': 'sec', 'siPrefix': True},
+            {'name': 'Type', 'type': 'list', 'values': ['Sync pulse', 'Generate train'], 'value': 'Sync pulse'},
+            {'name': 'Voltage', 'type': 'float', 'value': 2.0, 'step': 1.0, 'suffix': 'V'},
+            {'name': 'Voltage scale', 'type': 'float', 'value': 1.0, 'step': 1.0, 'suffix': 'V/V'},
+            {'name': 'Pulse rate', 'type': 'float', 'value': 75.0, 'step': 5.0, 'suffix': 'Hz'},
+        ]}
+    ]
+}
+
+parameterDefinitions = [
+    {'name': 'DAQ', 'type': 'group', 'children': [
+        {'name': 'Input', 'type': 'group', 'children': [
+            {'name': 'Sampling frequency', 'type': 'float', 'value': 10000.0, 'step': 500.0, 'siPrefix': True,
+             'suffix': 'Hz'},
+            {'name': 'Length', 'type': 'str', 'value': 'Dev1/ai0'},
+            {'name': 'Force', 'type': 'str', 'value': 'Dev1/ai1'},
+            {'name': 'Stim', 'type': 'str', 'value': 'Dev1/ai2'},
+            {'name': 'Force scale', 'type': 'float', 'value': 0.1, 'suffix': 'N/V'},
+            {'name': 'Sign convention', 'type': 'list',
+             'values': ['Lengthening is positive', 'Lengthening is negative', 'None'],
+             'value': 'Lengthening is positive'}
+        ]},
+        {'name': 'Output', 'type': 'group', 'children': [
+            {'name': 'Sampling frequency', 'type': 'float', 'value': 10000.0, 'step': 1000.0, 'siPrefix': True,
+             'suffix': 'Hz', 'readonly': True},
+            {'name': 'Stimulus', 'type': 'str', 'value': 'Dev1/ao0'},
+        ]},
+        {'name': 'Update rate', 'type': 'float', 'value': 10.0, 'suffix': 'Hz'}
+    ]},
+    {'name': 'Motor parameters', 'type': 'group', 'children': [
+        {'name': 'Length scale', 'type': 'float', 'value': 0.5, 'suffix': 'mm/V'},
+        {'name': 'Sign convention', 'type': 'list',
+         'values': ['Lengthening is positive', 'Lengthening is negative', 'None'],
+         'value': 'Lengthening is positive'}
+    ]},
+    {'name': 'Stimulus', 'type': 'group', 'children': [
+        {'name': 'Type', 'type': 'list', 'values': ['None', 'Sine', 'Frequency Sweep', 'Ramp'], 'value': 'Sine'},
+        {'name': 'Parameters', 'type': 'group', 'children': stimParameterDefs['Sine']},
+        {'name': 'Perturbations', 'type': 'group', 'children': [
+            {'name': 'On', 'type': 'bool', 'value': False},
+            {'name': 'Max amplitude', 'type': 'float', 'value': 5, 'suffix': '%'},
+            {'name': 'Amplitude scale', 'type': 'list', 'values': ['mm', '% fundamental'], 'value': '% fundamental'},
+            {'name': 'Amplitude frequency exponent', 'type': float, 'value': 0.25, 'step': 0.25,
+             'tip': 'Divide amplitudes by frequency to this exponent. 0 = no frequency scaling'},
+            {'name': 'Frequencies', 'type': 'string', 'value': ''},
+            {'name': 'Load frequencies...', 'type': 'action'},
+            {'name': 'Phases', 'type': 'string', 'value': ''},
+            {'name': 'Randomize phases...', 'type': 'action'},
+        ]},
+        {'name': 'Wait before', 'type': 'float', 'value': 1.0, 'suffix': 's'},
+        {'name': 'Wait after', 'type': 'float', 'value': 1.0, 'suffix': 's'},
+    ]}
+]
+
+def setup_parameters(params):
+    pass
