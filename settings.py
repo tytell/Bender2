afe9fc765eee0253ba154eaeda77cf3cb3579963
Eric Tytell <eric.tytell@tufts.edu>
2018-07-12 09:38:17 -0400

    Added settings file back in again

---
+++ settings.py
@@ -0,0 +1,9 @@
+SETTINGS_FILE = 'bender.ini'
+#EXPERIMENT_TYPE = 'isolated muscle'
+EXPERIMENT_TYPE = 'whole body'
+#MOTOR_TYPE = 'ergometer'
+#MOTOR_TYPE = 'velocity'
+MOTOR_TYPE = 'stepper'
+#COUNTER_TYPE = 'ergometer'
+COUNTER_TYPE = 'encoder'
+USE_ANALOG_STIM = True
